aa590051ee0204666d13415b076689da1a40f950
Graypawn <40048014+datapawn@users.noreply.github.com>
2021-04-16 12:59:09 -0700

    cleaned up comments

--- ex01/app.js
+++ ex01/app.js
@@ -1,8 +1,7 @@
+// ORIGINAL SPEC:
 // Write a function that takes a positive integer as an argument &
 // returns a Boolean indicating whether the argument is a power of three.
-
 // Hint: you might want to use recursion for this, which we haven't talked about yet. Exciting!
-
 // 3, 9, 27, 81...
 
 // Define UI Vars
